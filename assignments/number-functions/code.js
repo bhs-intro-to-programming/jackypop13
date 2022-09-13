b229829573be6c4ef2f4a4f512d5994146e60ec9
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-13 15:19:16 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -23,6 +23,6 @@ const distance=(a,b) => {
   return Math.abs(a-b)
 }
 
-const manhattanDistance=(x1,x2,y1,y2) => {
-  return x1+x2+y1+y2
+const manhattanDistance=(x1,y1,x2,y2) => {
+  return x1+y1+x2+y2
 }
