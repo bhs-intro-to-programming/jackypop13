ff7dded8909541838b1ec7fb5629e04e488e767f
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-14 14:58:54 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -30,3 +30,5 @@ const eatsWorms = (red, spotted) => red || spotted
 const eatsNuts = (red, spotted) => red || !spotted
 
 const eatsFish = (red, spotted) => !red || spotted
+
+const eatsMice = (red, spotted) => !red || !spotted
