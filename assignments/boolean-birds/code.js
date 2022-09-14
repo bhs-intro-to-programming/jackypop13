102a4834d98a1375924b0cb6aa17f0688ff33be8
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-14 15:14:32 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -39,7 +39,7 @@ const isSpotted = (s1) => {
   return s1 === 'Flobby' || s1 === 'Flibble'
 }
 const isNotRed = (s1) => {
-  return s1 === 'Flooby' || s1 === 'Flibble'
+  return s1 === 'Flobby' || s1 === 'Flibble'
 }
 const isNotSpotted = (s1) => {
   return s1 === 'Bloggy' || s1 === 'Globby'
