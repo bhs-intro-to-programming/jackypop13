442d7ea73f4b7bb0a87296a4c73d02d920623863
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-23 15:21:46 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -34,7 +34,7 @@ const drawLineCircle=(r) => {
 
 drawLineCircle(10)
 
-const drawLineCircle=(r) => {
+const drawCircle=(r) => {
   for (let i=0; i<width; i=i+2*r) {
     drawFilledCircle(r+i, height/2, r,'cyan')
   }
