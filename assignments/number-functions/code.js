a34c6b07d0e50b0ebedfc2cc40f4b5fe4247d4a1
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-28 15:25:51 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -25,3 +25,4 @@ const distance = (a, b) => {
 
 const manhattanDistance = (x1, y1, x2, y2) => distance(x1, x2) + distance(y1, y2)
 
+const euclideanDistance = (x1,y1,x2,y2) => distance(x1,x2)** + (distance(y1,y2)**2)
