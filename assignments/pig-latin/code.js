1f2fe23d93fd8865e5a015727290913a9982a9c2
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-29 15:25:41 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -32,8 +32,8 @@
 // Feel free to play around in the REPL to make sure you understand how this
 // operator works.
 
-const pigLatin=(s) => {s.substring( s.search (/[aeiou]/))  + s.substring(0,s.search (/[aeiou]/))+'ay' ;
+const pigLatin=(s) => s.substring( s.search (/[aeiou]/))  + s.substring(0,s.search (/[aeiou]/))+'ay' ;
 
-const advancedpigLatin=(i) => i=search (/[aeiou]/) 
+const advancedpigLatin=(i) => {i=search (/[aeiou]/) 
 return i===0 ? s + 'way':pigLatin(s);
 }
