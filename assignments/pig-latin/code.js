c81788ccefcffdd6948d2d4ac90253d3fca09282
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-29 15:27:39 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -34,7 +34,7 @@
 
 const pigLatin=(s) => s.substring( s.search (/[aeiou]/))  + s.substring(0,s.search (/[aeiou]/))+'ay' ;
 
-const advancedpigLatin=(i) => {
+const advancedpigLatin=(s) => {
 const i=s.search (/[aeiou]/) 
 return i===0 ? s + 'way':pigLatin(s);
 }
