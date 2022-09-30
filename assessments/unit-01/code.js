4eee8f2f995f5ae8c6c5d7aed525460dde32c930
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-09-30 15:19:54 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -5,7 +5,12 @@ const emit = (a, b, product) => {
 };
 
 // Write your code here ...
-const averageWeight=(a,b) => {
-  return (a*b)
+const averageWeight=(x,y) => (x/y)
   
+const hypotenuse=(a,b) => {
+  return  Math.sqrt(a**2 + b**2);
 }
+const maxRadius=(x,y) => {
+  return let( x<0) (width<)
+}
+const numCircles=()
