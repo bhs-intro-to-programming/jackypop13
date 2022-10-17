19b1bfa30866c67aa111ea865160b644cbb7dd60
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-10-17 15:13:14 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -9,3 +9,16 @@ const rand = (n) => Math.floor(Math.random() * n);
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+const fireAlarm=(alarmPulled,smokeDetected,plannedFireDrill) => {
+  return alarmPulled || smokeDetected || planedFireDrill
+}
+const canBePresident=(atLeast35,naturalBornCitizen,liveInUS14Years)=> {
+  return atLeast35 && naturalBornCitizen && liveInUS14Years
+}
+const willSeeTweet=(followTweeter,followRetweeter,noTweetIfblocked) => {
+  return followTweeter || followRetweeter || !noTweetIfblocked
+}
+const evenGreaterThanZero=(s)=> {
+  return 
+}
