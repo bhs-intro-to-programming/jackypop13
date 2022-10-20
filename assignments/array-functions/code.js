939495a0c9f652d72795ea30f42c86b09428a229
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-10-20 15:24:44 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -15,3 +15,12 @@ for (let i = 0; i < numbers.length; i++) {
 }
 return count
 }
+const even=(numbers)=>{
+let result = [];
+for (let i = 0; i < numbers.length; i++) {
+if (numbers[i] %2 === 0) {
+  result.push(numbers[i])
+}
+}
+return result
+}
