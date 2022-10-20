7011ae7d0e6f8ae2f24b0e08dc5a206e4eb9fbdf
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-10-20 15:12:50 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -8,7 +8,10 @@ for (let i = 0; i < numbers.length; i++) {
 }
 return count
 }
+const sum=(numbers)=>{
 let sum = 0;
 for (let i = 0; i < 100; i++) {
   sum = sum + something(i);
+}
+return count
 }
