241786a324a95768080f0ebeb7d004fc9a040881
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-10-27 15:24:22 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -8,5 +8,5 @@ const firstAndLast=(s)=>{
   return s.substring(0,1)+ s.substring(s.length -1)
 }
 const swapFronstAndBack=(s)=>{
-  return s.substring(s.length /2)
+  return s.substring(1)+s.substring(s.length /2)
 }
