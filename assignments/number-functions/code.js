de469841361b04c66f4d1166237585d3ad318ea0
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-11-02 15:30:25 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -19,7 +19,9 @@ const averageOfTwo=(a,b) => {
 const averageOfThree=(a,b,c) => {
   return (a+b+c)/3
 }
-
+const distance=(a,b)=>{
+  return (a+b)%2
+}
 
 const manhattanDistance=(x1,y1,x2,y2) => distance(x1,x2) + distance (y1,y2)
-euclideanDistance=(x1,y1,x2,y2) => distance(x1,x2) * distance (y1,y2)
+const euclideanDistance=(x1,y1,x2,y2) => distance(x1,x2) * distance (y1,y2)
