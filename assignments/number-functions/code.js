8eedebe66de77735c854e4ab8ebdbd2e2c1ef699
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-11-02 15:31:32 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,26 +1,26 @@
 const add = (a, b) => {
-  return 
+  return a + b
 }
 const subtract = (a, b) => {
-  return 
+  return a - b
 }
 const multiply = (a, b) => {
-  return 
+  return a * b
 }
 const divide = (a, b) => {
-  return
+  return a / b
 }
 const mod = (a, b) => {
-  return 
+  return a % b
 }
 const averageOfTwo = (a, b) => {
-  return 
+  return (a + b) / 2
 }
 const averageOfThree = (a, b, c) => {
-  return 
+  return (a + b + c) / 3
 }
 const distance = (a, b) => {
-  return 
+  return Math.abs(a - b)
 }
 
 const manhattanDistance = (x1, y1, x2, y2) => distance(x1, x2) + distance(y1, y2)
