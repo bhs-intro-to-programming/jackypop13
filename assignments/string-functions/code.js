1ca0ed226229d83dddd5cd8fa7ead927c152ddac
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-11-04 15:03:31 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -8,7 +8,7 @@ const firstAndLast=(s)=>{
   return s.substring(0,1)+ s.substring(s.length -1)
 }
 const swapFrontAndBack=(s)=>{
-  return s.substring(1)+s.substring(s.length /2)
+  return s.substring(s.length /2)+s.substring(0,s.length/2)
 }
 const simplePigLatin=(s)=>{
   return (s.substring(/[aeiou]/))+ay
