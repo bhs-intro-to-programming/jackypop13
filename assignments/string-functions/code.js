ed2bb35d406c1f29ecb62ff4b11ee79659a81b07
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-11-04 15:01:59 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -2,7 +2,7 @@ const findFnord=(s) => {
   return s.indexOf('fnord')
 }
 const stringContains=(s)=>{
-  return s.indexOf('fooooo')
+  return s.indexOf('foobar')
 }
 const firstAndLast=(s)=>{
   return s.substring(0,1)+ s.substring(s.length -1)
