d47eddf121199ae7e483deddb3e91048071a38d4
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-11-08 14:57:55 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -8,15 +8,15 @@ const emptyObject = () => {
   let object = {}
   return object
 }
-const distance = (p) => {
-  return (math.sqrt(math.abs, obj.x))
+const distance = (p1,p2) => {
+  return CCC
 }
 const midpoint = (p1, p2) => {
   //return { x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2 }
   return point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
 }
-const sumSalaries = () => {
-  return
+const sumSalaries = (saleries) => {
+  return object.saleries(+).obj
 }
 const newHighScore = () => {
   return
