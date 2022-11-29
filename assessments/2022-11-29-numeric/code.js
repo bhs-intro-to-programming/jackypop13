6ed0a6fd1e9c45c132e9890b22b0b71e33a79b3a
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-11-29 15:02:29 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -1,6 +1,27 @@
 const totalEggs=(hardBoiled,softBoiled)=>{
   return hardBoiled + softBoiled
 }
-const chocolatesPerPerson=(people,chocolates)=>{
+const chocolatesPerPerson=(numPeople,numChocolates)=>{
   return chocolates % people
+}
+const extrachocolates=(people,chocolates)=>{
+  return chocolates % people
+}
+const leftOut=()=>{
+  return
+}
+const probabilityAllHeads=()=>{
+  return
+}
+const futureHour=(curentHour,futureHour)=>{
+  return (curentHour * (futureHour%24))
+}
+const presentsBudget=(friends,avrgPresentCost)=>{
+  return friends * avrgPresentCost
+}
+const perPresent=(totalBudgeted,pretsentsNeedToBuy)=>{
+  return (totalBudgeted / pretsentsNeedToBuy)
+}
+const wrapingCombos=()=>{
+  return
 }
