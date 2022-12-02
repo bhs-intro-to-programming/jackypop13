4d347257480e0a35136bd41d629245a34ef4fd2b
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-12-02 15:22:38 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -8,9 +8,25 @@ const needMittens=(goingOutside,somewhereWarm)=>{
   return goingOutside || !somewhereWarm
 }
 const isVenomous=(striped,blueHead)=>{
-  return striped || blueHead
+  if (!striped && blueHead){
+  return false}
+  return true
 }
-const okaySpeed=(speedLimitMPH,carSpeedMPH)=>{
-   if (10 < speedLimit)|| (10>speedLimit)
-   return
+const okaySpeed=(speedLimit,carSpeed)=>{
+   if (speedLimit +10 >carSpeed && speedLimit -10 <carSpeed){
+   return true}
+   return false
 }
+const twiceAsExpensive=(p1,p2)=>{
+  return p1 > (p2*2)
+}
+const winningRecord=(wins,losses,ties)=>{
+  return wins > (losses+ties)
+}
+const  isMagicNumber=(num)=>{
+  if (num === 42){
+  if (num === 17)
+  return true}
+}
+//its a reference to hitchhickers guide to the galexy
+//i watched the movie it was kind of bad
