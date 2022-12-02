e3602a2900e232d4dc9476c251edf83d7c4ff5f5
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-12-02 15:00:10 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,4 +1,16 @@
 const needHeavyCoat=(goingOutside,somewhereCold)=>{
   return goingOutside && somewhereCold
 }
-const 
+const needSunscreen=(goingToBeach,goingSkiing)=>{
+  return goingToBeach || goingSkiing
+}
+const needMittens=(goingOutside,somewhereWarm)=>{
+  return goingOutside || !somewhereWarm
+}
+const isVenomous=(striped,blueHead)=>{
+  return striped || blueHead
+}
+const okaySpeed=(speedLimitMPH,carSpeedMPH)=>{
+   if (10 < speedLimit)|| (10>speedLimit)
+   return
+}
