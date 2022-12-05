d461b3ab9be38662b5d2543a7f4c36c3af3c0736
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-12-05 15:12:04 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -5,3 +5,21 @@ const upToX=(s)=>{
 const charactersAround=()=>{
   return 
 }
+const middle=(s1,s2)=>{
+  return
+}
+const pair=(string1,string2)=>{
+  return string1+'and'+string2
+}
+const containsX=(s1,s2,s3)=>{
+  return (s1.toLowerCase + s2.toLowerCase + s3.toLowerCase )+''
+}
+const slug=()=>{
+  return
+}
+const capitilize=()=>{
+  return
+}
+const capitalizeName=()=>{
+  return
+}
