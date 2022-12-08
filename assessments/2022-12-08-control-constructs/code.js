d30ec4069ae6903b7c5997db811d8105a8eb93d8
jackypop13 <111465878+jackypop13@users.noreply.github.com>
2022-12-08 15:09:44 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -1,12 +1,23 @@
 const yesIfEven=(a)=>{
-  if(a % 2) 
+  if(a % 2 === 0) 
+  return 'yes'
+  else
+  return 'no'
+}
+const countXs=(s)=>{
+  
+  
   return
 }
 const timesTable=()=>{
   return
 }
-const containsX=()=>{
-  return
+const containsX=(s)=>{
+  for(let i = 0 ; i<s.length;i++) {
+    consle.log(i)
+  if ([s]='x')
+  return true}
+  return false
 }
 const sumSquares=()=>{
   return
